c9bc7730c421a89ec5603a651f440f02025151c4
Ismael ElFatatry <ifatatry@Projecs.local>
2026-05-29 23:30:31 -0400

    Remove rate limit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express   = require('express');
 const mongoose  = require('mongoose');
 const cors      = require('cors');
-const rateLimit = require('express-rate-limit');
 
 const authRoutes   = require('./routes/auth');
 const habitsRoutes = require('./routes/habits');
@@ -11,7 +10,6 @@ const reportRoutes = require('./routes/reports');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(rateLimit({ windowMs: 15*60*1000, max: 200, legacyHeaders: false }));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
